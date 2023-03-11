90c58173076e0693c16c68a6e8dabb2a408ae58e
Merghen TM <jpbkghrmn@gmail.com>
2023-03-11 23:01:32 +0500

    6th commit

--- src/main/java/first_git_package/MyGitClass.java
+++ src/main/java/first_git_package/MyGitClass.java
@@ -4,7 +4,7 @@ public class MyGitClass {
 
     public static void main(String[] args) {
 
-
         System.out.println("Version1");
+        System.out.println("Changes");
     }
 }
--- src/main/java/first_git_package/MyThirdClass.java
+++ src/main/java/first_git_package/MyThirdClass.java
@@ -3,5 +3,6 @@ package first_git_package;
 public class MyThirdClass {
     public static void main(String[] args) {
         System.out.println("Third class");
+        System.out.println("For 6th commit");
     }
 }
